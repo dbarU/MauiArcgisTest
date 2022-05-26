8297156d90b0912748a0d920c21d5b01ef5fc3e8
Dvir Bar <dbar@ucsc.edu>
2022-05-25 18:01:01 -0700

    feat: Windows Platform works, Android does not work, not tested with iOS or MacCatalyst

--- MainPage.cs
+++ MainPage.cs
@@ -4,23 +4,20 @@ namespace MauiArcgisTest
 {
     using Esri.ArcGISRuntime.Geometry;
     using Esri.ArcGISRuntime.UI;
-    using Reactive.Bindings;
     using Map = Esri.ArcGISRuntime.Mapping.Map;
 
     public partial class MainPage : ContentPage
     {
-        public ReactiveProperty<Map> MyMap { get; } = new();
-        public ReactiveProperty<MapView> MyMapView { get; } = new();
-        public ReactiveProperty<SketchEditor> MySketchEditor { get; } = new(new SketchEditor());
+        private MapView _myMapView;
 
         public MainPage()
         {
-            MyMap.Value = new Esri.ArcGISRuntime.Mapping.Map(BasemapStyle.ArcGISChartedTerritory);
-            MySketchEditor.Value = new SketchEditor();
-            MyMapView.Value = new MapView()
+            var map = new Esri.ArcGISRuntime.Mapping.Map(BasemapStyle.ArcGISChartedTerritory);
+            var sketchEditor = new SketchEditor();
+            _myMapView = new MapView()
             {
-                Map = MyMap.Value,
-                SketchEditor = MySketchEditor.Value
+                Map = map,
+                SketchEditor = sketchEditor
             };
             Build();
         }
@@ -29,14 +26,13 @@ namespace MauiArcgisTest
             base.OnAppearing();
 
             await Task.Delay(5000);
-            await MyMapView.Value.SetViewpointAsync(new Viewpoint(new MapPoint(-117, 34, SpatialReferences.Wgs84), 1000000));
+            await _myMapView.SetViewpointAsync(new Viewpoint(new MapPoint(-117, 34, SpatialReferences.Wgs84), 1000000));
         }
         private void Build()
         {
             Content = new ScrollView()
             {
-                Content = MyMapView.Value
-
+                Content = _myMapView
             };
         }
 
